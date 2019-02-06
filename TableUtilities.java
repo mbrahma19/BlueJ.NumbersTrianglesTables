240ceea20508f61a7a48e10f84ceeddfa4af749b
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-8.local>
2019-02-06 18:59:20 -0500

    Completed Table Utilities

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,39 @@
- 
+
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String result = "";
+            for(int i = 1; i <= 5; i++){
+                for(int j = 1; j <= 5; j++){
+                    int s = i * j;
+                    result = result + String.format("%3d |",s);
+                }
+                result = result + "\n";
+            }
+        return result;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String result = "";
+            for(int i = 1; i <= 10; i++){
+                for(int j = 1; j <= 10; j++){
+                    int s = i * j;
+                    result = result + String.format("%3d |",s);
+                }
+                result = result + "\n";
+            }
+        return result;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result = "";
+            for(int i = 1; i <= tableSize; i++){
+                for(int j = 1; j <= tableSize; j++){
+                    int s = i * j;
+                    result = result + String.format("%3d |",s);
+                }
+                result = result + "\n";
+            }
+        return result;
     }
 }
